bcdde2fa8360a48acc722ba209f8dfdd50572483
uprashanth <uprashanth18@outlook.com>
2016-12-28 19:48:40 +0530

    Added action result.

---
+++ src/ComicBookGallery/Controllers/ComicBookController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBookController : Controller
+    {
+        public ActionResult Detail()
+        {
+            if(DateTime.Today.DayOfWeek == DayOfWeek.Wednesday)
+            {
+                return new RedirectResult("/");
+            }
+            return new ContentResult()
+            {
+                Content = "Hello World!"
+            };
+        }
+    }
+}
